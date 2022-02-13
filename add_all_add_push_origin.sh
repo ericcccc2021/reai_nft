c7be577a4659f70343f59c24e60826d4d4997619
ericcccc2021 <hxzhang.eric@gmail.com>
2022-02-12 22:27:43 -0800

    Some changes are made. This commit message is from a script so there is no meaningful information.

--- add_all_add_push_origin.sh
+++ add_all_add_push_origin.sh
@@ -1,3 +1,3 @@
 git add .
-git commit -m "some changes are made"
+git commit -m "Some changes are made. This commit message is from a script so there is no meaningful information."
 git push origin main
